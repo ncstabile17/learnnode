2dc79aefda16c94fc354807a37e6d7a5798e2a72
Nick Stabile <ncstabile17@gmail.com>
2017-03-26 17:42:46 +0000

    Processing HTTP GET

--- test-program.js
+++ test-program.js
@@ -7,6 +7,9 @@ var fs = require('fs');
 var file = process.argv[2];
 
   fs.readFile(file, function (err, fileContents) {
+      if (err) {
+          return console.log(err)
+      }
         var arrContents = fileContents.toString().split('\n');
         console.log(arrContents.length-1);
   });
