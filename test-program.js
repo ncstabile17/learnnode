5fdfb63cce5c2be928f3159a4a18b1dba9874d9a
Nick Stabile <ncstabile17@gmail.com>
2017-03-26 15:59:43 +0000

    File I/O handling

---
+++ test-program.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+// var contents = fs.readFileSync(process.argv[2])
+// var arrContents = contents.toString().split('\n')
+// console.log(arrContents.length-1)
+
+var file = process.argv[2];
+
+  fs.readFile(file, function (err, fileContents) {
+        var arrContents = fileContents.toString().split('\n');
+        console.log(arrContents.length-1);
+  });
