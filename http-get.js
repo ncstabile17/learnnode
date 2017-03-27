6de7d3a714a77ba817a0fb042484652457747d5c
Nick Stabile <ncstabile17@gmail.com>
2017-03-27 02:57:31 +0000

    Handling data streams from HTTP get

--- http-get.js
+++ http-get.js
@@ -1,10 +1,22 @@
 var http = require("http");
+var concat = require("concat-stream");
 
 var url = process.argv[2];
 
+// http.get(url, function(res) {
+//     res.setEncoding("utf8");
+//     res.on("data", function(data) {
+//         console.log(data);
+//     });
+// });
+
+
 http.get(url, function(res) {
-    res.setEncoding("utf8");
-    res.on("data", function(data) {
-        console.log(data);
-    });
+   // res.setEncoding("utf8");
+    res.pipe(concat(function(data) {
+        var str = data.toString();
+        var numChar = str.length;
+        console.log(numChar);
+        console.log(str);
+    }));
 });
